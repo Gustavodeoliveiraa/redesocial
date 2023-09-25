4ec46fdf7ac38ed14a79eef846233dd2eb92667e
rafaelrvs <rafaelvieira1333@gmail.com>
2023-09-25 00:08:18 -0300

    create branch c1

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('validate/', views.create_user, name='create'),
     path('login/', views.login_user, name='login'),
     path('process/form/', views.process_modal_form, name='form'),
+   
 ]
