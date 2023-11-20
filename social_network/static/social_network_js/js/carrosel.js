5a6780349ce42bef10ff1c8dbfdf10053b33dc82
Gustavodeoliveiraa <ggu3131@gmail.com>
2023-11-20 08:17:13 -0300

    status carousel fix

--- social_network/static/social_network_js/js/carrosel.js
+++ social_network/static/social_network_js/js/carrosel.js
@@ -1,42 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const controls = document.querySelectorAll(".control");
-    let currentItem = 0;
-    const items = document.querySelectorAll(".item");
-    const maxItems = items.length;
-  
-    if (maxItems == 1) {
-      items[currentItem].classList.remove("current-item");
-    }
-    controls.forEach((control) => {
-      control.addEventListener("click", (e) => {
-        isLeft = e.target.classList.contains("arrow-left");
-  
-        if (isLeft) {
-          currentItem -= 1;
-        } else {
-          currentItem += 1;
-        }
-  
-        if (currentItem >= maxItems) {
-          currentItem = 0;
-        }
-  
-        if (currentItem < 0) {
-          currentItem = maxItems - 1;
-        }
-  
-        items.forEach((item) => item.classList.remove("current-item"));
 
-        items[currentItem].scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
+    let currentItem = 0;
+
+    const divStatus = document.querySelector('.status')
+    const carrousel = document.querySelector(".carousel")
+
+    const controls = document.querySelectorAll(".control");
+    const item = document.querySelectorAll(".item");
+    const larguraItem = item[0].offsetWidth;
   
-        if (maxItems > 1) {
-          items[currentItem].classList.remove("current-item");
-        }
+    const maxItems = item.length - 1;
+
+
+    if (carrousel.offsetWidth > divStatus.offsetWidth){
+      controls.forEach((control) => {
+        control.addEventListener("click", (e) => {
+          isLeft = e.target.classList.contains("arrow-left");
+    
+          
+  
+            if (isLeft) {
+              if (currentItem <= 0){
+                currentItem = maxItems 
+              }
+              else {
+                currentItem -= 1;
+              }
+              carrousel.style.transform = `translateX(-${currentItem * larguraItem}px)`
+    
+            } else {
+  
+              if (currentItem > maxItems - 1) {
+                currentItem = 0
+              }
+              else {
+                currentItem += 1;
+              }
+              carrousel.style.transform = `translateX(-${currentItem * larguraItem}px)`
+    
+            }
+  
+            item.forEach((items)=>items.classList.remove('current-item'))
+            item[currentItem].classList.add('current-item')
+        });
       });
-    });
+    }else{
+      item.forEach((items)=>items.classList.add('current-item'))
+      controls.forEach((element)=>element.style.display = 'none')
+    }
+    
 
     // hiding the status div if it has no status
 
