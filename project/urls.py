7af2e43af7d27910b618a5e87a82f6582e3f2584
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-02-14 00:52:31 -0300

    Setting up the server to serve the static files

--- project/urls.py
+++ project/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
         'reset-password/complete/', 
         auth_views.PasswordResetCompleteView.as_view(),
         name='password_reset_complete'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += path("__debug__/", include("debug_toolbar.urls")),
